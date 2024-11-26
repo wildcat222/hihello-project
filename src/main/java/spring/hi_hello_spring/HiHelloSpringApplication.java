91b77b2f4679c31fceaaf303350d433436f0367e
kookong2 <hy010309@naver.com>
2024-11-26 14:19:13 +0900

    [FEAT] 스프링 프로젝트 수정 작업

---
+++ src/main/java/spring/hi_hello_spring/HiHelloSpringApplication.java
@@ -0,0 +1,13 @@
+package spring.hi_hello_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiHelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiHelloSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/hi_hello_spring/HiHelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.hi_hello_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiHelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
