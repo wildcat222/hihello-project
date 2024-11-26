6678d8b1c50cf6c2055bf4958d24018864dfe800
kookong2 <hy010309@naver.com>
2024-11-26 17:24:50 +0900

    [FEAT] @EnableJpaAuditing 추가

--- src/main/java/spring/hi_hello_spring/HiHelloSpringApplication.java
+++ src/main/java/spring/hi_hello_spring/HiHelloSpringApplication.java
@@ -2,8 +2,10 @@ package spring.hi_hello_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HiHelloSpringApplication {
 
 	public static void main(String[] args) {
