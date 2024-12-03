d0dbe71df3b256456c87f67dd735a4f6a94d81ca
kookong2 <hy010309@naver.com>
2024-12-03 15:12:00 +0900

    [FEAT] 동료 평가 지표 삭제 기능 구현

--- src/main/java/spring/hi_hello_spring/group/command/application/controller/PeerReviewListController.java
+++ src/main/java/spring/hi_hello_spring/group/command/application/controller/PeerReviewListController.java
@@ -3,9 +3,7 @@ package spring.hi_hello_spring.group.command.application.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spring.hi_hello_spring.common.response.ApiResponse;
 import spring.hi_hello_spring.common.response.ResponseUtil;
 import spring.hi_hello_spring.group.command.application.dto.PeerReviewListCreateDTO;
@@ -25,5 +23,13 @@ public class PeerReviewListController {
 
         peerReviewListService.createPeerReviewList(createDTO);
         return ResponseUtil.successResponse("동료 평가 지표가 성공적으로 등록되었습니다.").getBody();
+    }
+
+    @DeleteMapping("/{peerReviewListSeq}")
+    @Operation(summary = "동료 평가 지표 삭제", description = "동료 평가 지표 삭제 로직입니다.")
+    public ApiResponse<?> deletePeerReviewList(@PathVariable Long peerReviewListSeq){
+
+        peerReviewListService.deletePeerReviewList(peerReviewListSeq);
+        return ResponseUtil.successResponse("동료 평가 지표가 성공적으로 삭제 되었습니다.").getBody();
     }
 }
