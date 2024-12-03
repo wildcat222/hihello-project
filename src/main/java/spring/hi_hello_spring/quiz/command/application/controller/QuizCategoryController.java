41c9aee6ec99e0c3164243f55b80492485309f5f
kookong2 <hy010309@naver.com>
2024-12-03 22:09:30 +0900

    [FEAT] 퀴즈 카테고리 삭제 기능 구현

--- src/main/java/spring/hi_hello_spring/quiz/command/application/controller/QuizCategoryController.java
+++ src/main/java/spring/hi_hello_spring/quiz/command/application/controller/QuizCategoryController.java
@@ -3,9 +3,7 @@ package spring.hi_hello_spring.quiz.command.application.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spring.hi_hello_spring.common.response.ApiResponse;
 import spring.hi_hello_spring.common.response.ResponseUtil;
 import spring.hi_hello_spring.quiz.command.application.dto.QuizCategoryCreateDTO;
@@ -26,5 +24,14 @@ public class QuizCategoryController {
 
         quizCategoryService.createQuizCategory(createDTO);
         return ResponseUtil.successResponse("퀴즈 카테고리가 성공적으로 등록되었습니다.").getBody();
+    }
+
+    /* 퀴즈 카테고리 삭제 */
+    @DeleteMapping("/{quizCategorySeq}")
+    @Operation(summary = "퀴즈 카테고리 삭제", description = "퀴즈 카테고리 삭제 로직입니다.")
+    public ApiResponse<?> deleteQuizCategory(@PathVariable Long quizCategorySeq){
+
+        quizCategoryService.deleteQuizCategory(quizCategorySeq);
+        return ResponseUtil.successResponse("퀴즈 카테고리가 성공적으로 삭제되었습니다.").getBody();
     }
 }
