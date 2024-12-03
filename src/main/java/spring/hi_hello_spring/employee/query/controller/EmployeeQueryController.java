4cad0803b4e0fdb8ab2a88e7071cf347eb3179c6
minju <kminju0224@gmail.com>
2024-12-03 14:49:51 +0900

    [FEAT] 부서별 멘티 조회 기능

--- src/main/java/spring/hi_hello_spring/employee/query/controller/EmployeeQueryController.java
+++ src/main/java/spring/hi_hello_spring/employee/query/controller/EmployeeQueryController.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.hi_hello_spring.common.response.ApiResponse;
 import spring.hi_hello_spring.common.response.ResponseUtil;
 import spring.hi_hello_spring.employee.query.dto.MenteeAllQueryDTO;
+import spring.hi_hello_spring.employee.query.dto.MenteeDepQueryDTO;
 import spring.hi_hello_spring.employee.query.service.EmployeeQueryService;
 
 import java.util.List;
@@ -27,5 +29,13 @@ public class EmployeeQueryController {
 
         List<MenteeAllQueryDTO> menteeAllQueryDTO = employeeQueryService.getAllEmployee();
         return ResponseUtil.successResponse("멘티 전체 조회가 성공적으로 조회되었습니다.", menteeAllQueryDTO).getBody();
+    }
+
+    @GetMapping("/mentee/{departmentSeq}")
+    @Operation(summary = "부서별 멘티 조회", description = "부서별 멘티 조회 로직 입니다.")
+    public ApiResponse<?> getEmployeeByDepartmentSeq( @PathVariable Long departmentSeq) {
+
+        List<MenteeDepQueryDTO> menteeDepQueryDTO = employeeQueryService.getDepMentees(departmentSeq);
+        return ResponseUtil.successResponse("부서별 멘티 조회가 성공적으로 조회되었습ㄴ니다.", menteeDepQueryDTO).getBody();
     }
 }
