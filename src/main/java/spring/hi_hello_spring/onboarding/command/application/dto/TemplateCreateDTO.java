5213f1ea61954183f9a488b58fb40acc018d53e9
kookong2 <hy010309@naver.com>
2024-12-04 16:55:43 +0900

    [FEAT] 온보딩 스토리 보드 등록 기능 구현 중

---
+++ src/main/java/spring/hi_hello_spring/onboarding/command/application/dto/TemplateCreateDTO.java
@@ -0,0 +1,31 @@
+package spring.hi_hello_spring.onboarding.command.application.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import spring.hi_hello_spring.onboarding.command.domain.aggregate.entity.TemplateType;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class TemplateCreateDTO {
+
+    private Long quizCategorySeq;
+    private TemplateType templateType;
+    private Boolean templateCheckRequiredStatus;
+    private String templateTrainingType;
+    private String templateTitle;
+    private String templateSub;
+    private String templateDetail;
+    private String templateUrl;
+    private int templateQuizQty;
+    private String templateTaskRound;
+    private int templateProcedure;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private LocalDateTime templateEndAt;
+}
---
+++ src/main/java/spring/hi_hello_spring/onboarding/command/domain/repository/TemplateRepository.java
@@ -0,0 +1,7 @@
+package spring.hi_hello_spring.onboarding.command.domain.repository;
+
+import spring.hi_hello_spring.onboarding.command.domain.aggregate.entity.Template;
+
+public interface TemplateRepository {
+    Template save(Template template);
+}
