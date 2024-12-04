241188647efc4b3fe9c5f5c99a1d99d1d036840f
WhooGeek <rlarjswn0303@naver.com>
2024-12-05 01:21:08 +0900

    [FEAT] TaskQueryService 멘토 과제 상세조회 기능 수정

--- src/main/java/spring/hi_hello_spring/evaluation/query/service/TaskQueryService.java
+++ src/main/java/spring/hi_hello_spring/evaluation/query/service/TaskQueryService.java
@@ -23,7 +23,7 @@ public class TaskQueryService {
     public List<TaskMenteeDetailQueryDTO> getMenteeTaskDetail(Long taskSeq) {
 
         Map<String, Object> params = new HashMap<>();
-        params.put("employee_seq", 5L);
+        params.put("employee_seq", 5L); // 시큐리티Seq 구현되면 변경 예정
         params.put("task_seq", taskSeq);
 
         return taskQueryMapper.findMenteeTaskDetail(params);
