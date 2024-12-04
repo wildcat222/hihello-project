ebdbfa918bcb4a2390535685643ba12fe3cec045
WhooGeek <rlarjswn0303@naver.com>
2024-12-04 21:53:19 +0900

    [FEAT] TaskQueryController 멘토 과제 상세조회 기능 구현

--- src/main/java/spring/hi_hello_spring/evaluation/query/controller/TaskQueryController.java
+++ src/main/java/spring/hi_hello_spring/evaluation/query/controller/TaskQueryController.java
@@ -4,12 +4,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.hi_hello_spring.common.response.ApiResponse;
 import spring.hi_hello_spring.common.response.ResponseUtil;
 import spring.hi_hello_spring.employee.query.dto.MenteeAllQueryDTO;
 import spring.hi_hello_spring.evaluation.query.dto.TaskHrAllListQueryDTO;
+import spring.hi_hello_spring.evaluation.query.dto.TaskMenteeDetailQueryDTO;
 import spring.hi_hello_spring.evaluation.query.service.TaskQueryService;
 
 import java.util.List;
@@ -28,5 +30,13 @@ public class TaskQueryController {
 
         List<TaskHrAllListQueryDTO> taskHrAllListQueryDTO = taskQueryService.getHrAllTaskList();
         return ResponseUtil.successResponse("멘티 전체 조회가 성공적으로 조회되었습니다.", taskHrAllListQueryDTO).getBody();
+    }
+
+    @GetMapping("mentee/task/{taskSeq}")
+    @Operation(summary = "멘토 과제 상세 조회", description = "멘토가 과제를 상세조회하는 기능입니다.")
+    public ApiResponse<?> getMenteeTaskDetail(@PathVariable Long taskSeq) {
+
+        List<TaskMenteeDetailQueryDTO> taskMenteeDetailQueryDTO = taskQueryService.getMenteeTaskDetail(taskSeq);
+        return ResponseUtil.successResponse("멘티 전체 조회가 성공적으로 조회되었습니다.", taskMenteeDetailQueryDTO).getBody();
     }
 }
