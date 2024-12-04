a6c8d9de1244d8c47de66c45d8a6acc12211859e
WhooGeek <rlarjswn0303@naver.com>
2024-12-04 21:53:27 +0900

    [FEAT] TaskQueryMapper 멘토 과제 상세조회 기능 구현

--- src/main/java/spring/hi_hello_spring/evaluation/query/mapper/TaskQueryMapper.java
+++ src/main/java/spring/hi_hello_spring/evaluation/query/mapper/TaskQueryMapper.java
@@ -2,12 +2,15 @@ package spring.hi_hello_spring.evaluation.query.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import spring.hi_hello_spring.evaluation.query.dto.TaskHrAllListQueryDTO;
+import spring.hi_hello_spring.evaluation.query.dto.TaskMenteeDetailQueryDTO;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface TaskQueryMapper {
 
     List<TaskHrAllListQueryDTO> findHrAllTask();
 
+    List<TaskMenteeDetailQueryDTO> findMenteeTaskDetail(Map<String, Object> params);
 }
