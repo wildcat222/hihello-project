59963c5dd9da3e5f78cf1780dbcb6ef4b0873a4f
WhooGeek <rlarjswn0303@naver.com>
2024-12-04 21:53:34 +0900

    [FEAT] TaskQueryService 멘토 과제 상세조회 기능 구현

--- src/main/java/spring/hi_hello_spring/evaluation/query/service/TaskQueryService.java
+++ src/main/java/spring/hi_hello_spring/evaluation/query/service/TaskQueryService.java
@@ -3,9 +3,12 @@ package spring.hi_hello_spring.evaluation.query.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import spring.hi_hello_spring.evaluation.query.dto.TaskHrAllListQueryDTO;
+import spring.hi_hello_spring.evaluation.query.dto.TaskMenteeDetailQueryDTO;
 import spring.hi_hello_spring.evaluation.query.mapper.TaskQueryMapper;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +18,15 @@ public class TaskQueryService {
     public List<TaskHrAllListQueryDTO> getHrAllTaskList() {
 
         return taskQueryMapper.findHrAllTask();
+    }
+
+    public List<TaskMenteeDetailQueryDTO> getMenteeTaskDetail(Long taskSeq) {
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("employee_seq", 1L);
+        params.put("task_seq", taskSeq);
+
+        return taskQueryMapper.findMenteeTaskDetail(params);
+
     }
 }
