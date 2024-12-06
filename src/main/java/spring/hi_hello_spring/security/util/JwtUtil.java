eb64d836f56dd01f80343d1bb7dbd9840b097413
wildcat222 <wildcat222@naver.com>
2024-12-06 19:18:55 +0900

    [CHORE] 간단한 주석 정리

--- src/main/java/spring/hi_hello_spring/security/util/JwtUtil.java
+++ src/main/java/spring/hi_hello_spring/security/util/JwtUtil.java
@@ -60,19 +60,9 @@ public class JwtUtil {
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken);
             return true;
-        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
-            log.info("Invalid JWT Token {}", e);
-        } catch (ExpiredJwtException e) {
-            log.info("Expired JWT Token {}", e);
-        } catch (UnsupportedJwtException e) {
-            log.info("Unsupported JWT Token {}", e);
-        } catch (IllegalArgumentException e) {
-            log.info("JWT Token claims empty {}", e);
+        } catch (Exception e) {
+            return false;
         }
-//        catch (Exception e) {
-//            return false;
-//        }
-        return false;
     }
 
     /* refreshToken 검증(Bearer 토큰이 넘어왔고, 우리 사이트의 secret key로 만들어 졌는가, 만료되었는지와 내용이 비어있진 않은가
