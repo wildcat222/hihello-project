e3460fd64c4aa89f40169bba871b33562d188949
WhooGeek <rlarjswn0303@naver.com>
2024-12-06 14:31:16 +0900

    [FEAT] TaskQueryService 코드 추가

--- src/main/java/spring/hi_hello_spring/evaluation/query/service/TaskQueryService.java
+++ src/main/java/spring/hi_hello_spring/evaluation/query/service/TaskQueryService.java
@@ -3,6 +3,7 @@ package spring.hi_hello_spring.evaluation.query.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import spring.hi_hello_spring.evaluation.query.dto.TaskMenteeDetailQueryDTO;
+import spring.hi_hello_spring.evaluation.query.dto.TaskMentorDetailQueryDTO;
 import spring.hi_hello_spring.evaluation.query.dto.TaskAllListQueryDTO;
 import spring.hi_hello_spring.evaluation.query.mapper.TaskQueryMapper;
 
@@ -20,18 +21,26 @@ public class TaskQueryService {
         return taskQueryMapper.findHrAllTask();
     }
 
-    public List<TaskMenteeDetailQueryDTO> getMenteeTaskDetail(Long taskSeq) {
+    public List<TaskMentorDetailQueryDTO> getMentorTaskDetail(Long taskSeq) {
 
         Map<String, Object> params = new HashMap<>();
-        params.put("employee_seq", 5L); // 시큐리티Seq 구현되면 변경 예정
+        params.put("employee_seq", 6L); // 시큐리티Seq 구현되면 변경 예정
         params.put("task_seq", taskSeq);
 
-        return taskQueryMapper.findMenteeTaskDetail(params);
+        return taskQueryMapper.findMentorTaskDetail(params);
     }
 
     public List<TaskAllListQueryDTO> getMentorAllTaskList() {
         Long employeeSeq = 3L; // 로그인 기능 완성되면 코드 수정하겠습니다.
         return taskQueryMapper.findMentorAllTask(employeeSeq);
+    }
+
+    public List<TaskMenteeDetailQueryDTO> getMenteeTaskDetail(Long taskSeq) {
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("employee_seq", 2L); // 시큐리티Seq 구현되면 변경 예정
+        params.put("task_seq", taskSeq);
+        return taskQueryMapper.findMenteeTaskDetail(params);
     }
 }
 
