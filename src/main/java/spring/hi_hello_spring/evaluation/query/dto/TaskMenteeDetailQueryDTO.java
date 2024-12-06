3a4885e17a298ec67340e61075d341177da44cf0
WhooGeek <rlarjswn0303@naver.com>
2024-12-06 14:30:32 +0900

    [FEAT] TaskMenteeDetailQueryDTO 코드 추가

--- src/main/java/spring/hi_hello_spring/evaluation/query/dto/TaskMenteeDetailQueryDTO.java
+++ src/main/java/spring/hi_hello_spring/evaluation/query/dto/TaskMenteeDetailQueryDTO.java
@@ -14,11 +14,13 @@ import java.time.LocalDateTime;
 public class TaskMenteeDetailQueryDTO {
 
     private Long taskSeq;
+    private String templateTitle;
     private String taskType;
     private Long templateSeq;
-    private String templateTitle;
-    private String templateUrl;
     private String taskContent;
+    private String taskUrl;
+    private Long departmentSeq;
+    private String departmentName;
     private String templateTaskRound;
     private LocalDateTime templateEndAt;
 }
