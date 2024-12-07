84b9595b3632fa990d1c03f903a415b92544d27c
WhooGeek <rlarjswn0303@naver.com>
2024-12-07 18:25:00 +0900

    [FEAT] Service 수정

--- src/main/java/spring/hi_hello_spring/evaluation/command/application/service/TaskService.java
+++ src/main/java/spring/hi_hello_spring/evaluation/command/application/service/TaskService.java
@@ -6,15 +6,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spring.hi_hello_spring.evaluation.command.application.dto.TaskCreateDTO;
-import spring.hi_hello_spring.evaluation.command.domain.aggregate.entity.EvalList;
 import spring.hi_hello_spring.evaluation.command.domain.aggregate.entity.Task;
 import spring.hi_hello_spring.evaluation.command.domain.repository.EvalListRepository;
 import spring.hi_hello_spring.evaluation.command.domain.repository.TaskRepository;
-import spring.hi_hello_spring.onboarding.command.domain.aggregate.entity.Template;
+import spring.hi_hello_spring.evaluation.command.domain.service.EvalListDomainService;
 import spring.hi_hello_spring.onboarding.command.domain.repository.TemplateRepository;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import spring.hi_hello_spring.onboarding.command.domain.service.TemplateDomainService;
 
 @Slf4j
 @Service
@@ -25,36 +22,22 @@ public class TaskService {
     private final TaskRepository taskRepository;
     private final TemplateRepository templateRepository;
     private final ModelMapper modelMapper;
+    private final EvalListDomainService evalListDomainService;
+    private final TemplateDomainService templateDomainService;
 
     // 과제 등록
     @Transactional
     public void createTask(TaskCreateDTO taskContent) {
 
-        // Task 항목 저장
         Task task = new Task(taskContent);
-        taskRepository.save(task);  // task 테이블에 저장 후, taskSeq가 자동 생성됨
+        taskRepository.save(task);
 
         // EvalList 항목들 저장
-        List<EvalList> evalIndicators = taskContent.getEvalIndicators().stream()
-                .map(dto -> {
-                    EvalList evalList = modelMapper.map(dto, EvalList.class);
-                    evalList.updateTaskSeq(task.getTaskSeq());  // taskSeq 값을 설정
-                    return evalList;
-                })
-                .collect(Collectors.toList());
-        evalListRepository.saveAll(evalIndicators);  // eval_list 테이블에 저장
+        evalListDomainService.createTask(taskContent,task);
 
         // Template 항목 수정
-        Template template = templateRepository.findByTemplateTaskRound(
-                taskContent.getTemplateTaskRound()
-        );
-        if (template == null) {
-            throw new RuntimeException("Template not found");
-        }
+        templateDomainService.updateTask(taskContent);
 
-        // Template 업데이트 (DTO에 기반하여 업데이트)
-        template.updateTemplate(taskContent.getTemplateUrl());
-        templateRepository.save(template);  // template 테이블에 수정된 내용 저장
     }
 
     // 과제 수정
