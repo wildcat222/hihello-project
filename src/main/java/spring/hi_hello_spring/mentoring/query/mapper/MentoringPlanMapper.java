02e04219daaf6fadb9c3fe553e112f580234e1f0
minju <kminju0224@gmail.com>
2024-12-09 16:40:56 +0900

    [FEAT] 멘토링 계획서 검색(name, title)

--- src/main/java/spring/hi_hello_spring/mentoring/query/mapper/MentoringPlanMapper.java
+++ src/main/java/spring/hi_hello_spring/mentoring/query/mapper/MentoringPlanMapper.java
@@ -3,6 +3,7 @@ package spring.hi_hello_spring.mentoring.query.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import spring.hi_hello_spring.mentoring.query.dto.MentoringPlanDetailDTO;
 import spring.hi_hello_spring.mentoring.query.dto.MentoringPlanListAllQueryDTO;
+import spring.hi_hello_spring.mentoring.query.dto.MentoringPlanSearchDTO;
 
 import java.util.List;
 
@@ -11,4 +12,6 @@ public interface MentoringPlanMapper {
     List<MentoringPlanListAllQueryDTO> findAllMentoringPlanList();
 
     MentoringPlanDetailDTO findMentoringPlanDetail(Long planningSeq);
+
+    List<MentoringPlanSearchDTO> findMentoringPlanSearch(String ctg, String word);
 }
