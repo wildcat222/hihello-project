9e6e25ffc8f29ec0018e404e5175f932d48af221
hyomee2 <s5763305@naver.com>
2024-12-10 00:32:19 +0900

    [FEAT] 과제 평가 상세조회 기능 구현

--- src/main/java/spring/hi_hello_spring/evaluation/query/mapper/TaskEvalMapper.java
+++ src/main/java/spring/hi_hello_spring/evaluation/query/mapper/TaskEvalMapper.java
@@ -1,6 +1,7 @@
 package spring.hi_hello_spring.evaluation.query.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import spring.hi_hello_spring.evaluation.query.dto.TaskEvalDetailsQueryDTO;
 import spring.hi_hello_spring.evaluation.query.dto.TaskEvalListQueryDTO;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface TaskEvalMapper {
 
     List<TaskEvalListQueryDTO> findAllTaskEvals();
+
+    List<TaskEvalDetailsQueryDTO> findTaskEvalDetails(Long taskSubmitSeq);
 }
