bc67f28997b8c6bf51554d219f7b3bc825e5e3eb
kookong2 <hy010309@naver.com>
2024-12-10 15:07:55 +0900

    [FEAT] WhiteBox 컴포넌트 생성 작업

---
+++ src/components/WhiteBoxComponet.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="white-box">
+      <slot></slot>
+    </div>
+</template>
+  
+<style scoped>
+  .white-box {
+    background-color: white;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    margin: 10px;
+    width: 100%; 
+    max-width: 600px; 
+    text-align: center; 
+  }
+</style>
+  
