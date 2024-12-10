e93c1147df81d13ed8af8971b5d56cf4b00940c9
wildcat222 <wildcat222@naver.com>
2024-12-10 19:52:39 +0900

    [FEAT] 온보딩 리스트 조회 기능 구현

---
+++ src/main/java/spring/hi_hello_spring/onboarding/query/dto/OnboardingDTO.java
@@ -0,0 +1,44 @@
+package spring.hi_hello_spring.onboarding.query.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
+public class OnboardingDTO {
+
+    private Long employeeSeq;
+    private String employeeName;
+    private Long onboardingStatusSeq;
+    private boolean onboardingCompletedStatus = false;
+
+    private Long templateSeq;
+    private String templateType;
+    private boolean templateCheckRequiredStatus;
+    private String templateTrainingType;
+    private String templateTitle;
+    private String templateSub;
+    private String templateDetail;
+    private String templateUrl;
+
+    private Long quizCategorySeq;
+    private String quizCategoryName;
+
+    private String templateTaskRound;
+    private Long taskSeq;
+    private String taskTitle;
+
+    private int templateProcedure;
+    private LocalDateTime templateEndAt;
+
+    private Long checklistSeq;
+    private String checklistContent;
+    private Long checklistStatusSeq;
+    private boolean listCheckedStatus;
+}
