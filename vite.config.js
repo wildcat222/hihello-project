779b16f7371586e05bf87e15067dc07b91a90329
kookong2 <hy010309@naver.com>
2024-12-11 12:01:22 +0900

    [REFACTOR] 프록시 설정 제거

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,6 @@ dotenv.config();
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_BASE_URL || 'http://localhost:8253', // 환경 변수 읽기
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
