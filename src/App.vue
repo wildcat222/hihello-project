5daf0cfb6e7551b271d57d2c3590b6230dd7241e
kookong2 <hy010309@naver.com>
2024-12-12 16:25:42 +0900

    [FEAT] userStore 적용 작업

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import AsideComponent from '@/components/AsideComponent.vue';
+import { useUserStore } from '@/stores/UserStore';
+import { springAPI } from '@/services/axios.js';
 
 const route = useRoute();
+const userStore = useUserStore();
 
-// 반응형으로 `shouldShowAside`를 계산
 const shouldShowAside = computed(() => !route.meta.hideAside);
+
+onMounted(() => {
+    if (userStore.accessToken) {
+        springAPI.defaults.headers.common['Authorization'] = `Bearer ${userStore.accessToken}`;
+        userStore.isAuthenticated = true;
+    }
+
+    // Axios 인터셉터 초기화
+    userStore.initializeInterceptors();
+});
 </script>
 
 <template>
   <div id="app">
-    <!-- 조건부 렌더링 -->
+    <!-- AsideComponent 렌더링 여부 결정 -->
     <AsideComponent v-if="shouldShowAside" />
     <div class="router-container">
       <router-view />
