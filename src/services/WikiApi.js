bf381d5d3cc37ea6cb5faf96814253df2274c2c0
hyomee2 <s5763305@naver.com>
2024-12-13 02:34:24 +0900

    [FEAT] 위키 수정 에디터 연동 및 백엔드 연동

--- src/services/WikiApi.js
+++ src/services/WikiApi.js
@@ -1,10 +1,30 @@
 import axios from 'axios';
+import {springAPI} from "@/services/axios.js";
 
+// 위키 등록
 export const createWiki = async(wikiData) => {
     try {
-        const response =  await axios.post(`http://localhost:8253/api/v1/wiki`, wikiData);
-        return response.data;
+        return await axios.post(`http://localhost:8253/api/v1/wiki`, wikiData);
     } catch(error) {
         console.error("위키를 등록하던 중 오류가 발생했습니다.", error);
+    }
+}
+
+// 위키 조회
+export const fetchWiki = async(wikiSeq) => {
+    try {
+        const response = await axios.get(`http://localhost:8253/api/v1/wiki/${wikiSeq}`);
+        return response.data;
+    } catch(error) {
+        console.error('위키 내용을 가져오는 중 오류가 발생헸습니다.', error);
+    }
+}
+
+// 위키 수정
+export const updateWiki = async(wikiSeq, wikiData) => {
+    try {
+        return await axios.put(`http://localhost:8253/api/v1/wiki/${wikiSeq}`, wikiData);
+    } catch(error) {
+        console.error("위키를 수정하던 중 오류가 발생했습니다.", error);
     }
 }
