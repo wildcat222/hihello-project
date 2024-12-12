ec93fcb2d3e4d2ca16a732b9004c9369947ae932
WhooGeek <rlarjswn0303@naver.com>
2024-12-12 22:24:50 +0900

    [CHORE] Task 안쓰는 import 제거

--- src/main/java/spring/hi_hello_spring/evaluation/command/domain/aggregate/entity/Task.java
+++ src/main/java/spring/hi_hello_spring/evaluation/command/domain/aggregate/entity/Task.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.SQLDelete;
 import spring.hi_hello_spring.common.aggregate.entity.BaseTimeEntity;
 
 @Entity
