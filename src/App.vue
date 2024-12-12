34210f2f515130e647355de80c7f61e2fed6496d
kookong2 <hy010309@naver.com>
2024-12-12 12:19:39 +0900

    [REFACTOR] AsideComponent 조건부 렌더링 작업

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 import AsideComponent from '@/components/AsideComponent.vue';
+
+const route = useRoute();
+
+// 반응형으로 `shouldShowAside`를 계산
+const shouldShowAside = computed(() => !route.meta.hideAside);
 </script>
 
 <template>
   <div id="app">
-    <AsideComponent />
+    <!-- 조건부 렌더링 -->
+    <AsideComponent v-if="shouldShowAside" />
     <div class="router-container">
       <router-view />
     </div>
@@ -21,4 +29,3 @@ import AsideComponent from '@/components/AsideComponent.vue';
   background-color: var(--ivory);
 }
 </style>
-
