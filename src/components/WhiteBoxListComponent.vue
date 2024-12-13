69a21051ab57ffde27e6b1d8df0cd59365c5ad4d
hyomee2 <s5763305@naver.com>
2024-12-13 15:30:34 +0900

    [FEAT] 리스트 컴포넌트 구현

---
+++ src/components/WhiteBoxListComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="white-box">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.white-box {
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  padding: 40px;
+  width: 100%;
+  max-width: 1280px;
+  text-align: center;
+}
+</style>
