a6a7b6408613870de989ba5a715227d6227bba43
kookong2 <hy010309@naver.com>
2024-12-15 23:57:51 +0900

    [FEAT] 멘토 소개 페이지 생성

---
+++ src/views/user/MentorInfoPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>멘토 소개</h1>
+        <white-box>
+
+        </white-box>
+    </div>
+</template>
+
+<script setup>
+import WhiteBox from "@/components/WhiteBoxComponent.vue";
+
+</script>
