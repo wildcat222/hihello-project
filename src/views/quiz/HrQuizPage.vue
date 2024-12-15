4be9a7661a3f633a831a3a50b30f12a4be67733a
kookong2 <hy010309@naver.com>
2024-12-16 00:33:32 +0900

    [FEAT] 퀴즈 카테고리 컴포넌트 퍼블리싱 및 api 연동 작업

--- src/views/quiz/HrQuizPage.vue
+++ src/views/quiz/HrQuizPage.vue
@@ -3,6 +3,7 @@
         <h1>퀴즈 조회</h1>
         <search-bar></search-bar>
         <button>퀴즈 등록</button>
+        <quiz-category></quiz-category>
         <white-box>
             <list-component></list-component>
         </white-box>
@@ -13,4 +14,5 @@
 import SearchBar from '@/components/SearchBarComponent.vue';
 import WhiteBox from '@/components/WhiteBoxComponent.vue';
 import ListComponent from '@/components/ListComponent.vue';
+import QuizCategory from '@/components/QuizCategoryComponent.vue';
 </script>
