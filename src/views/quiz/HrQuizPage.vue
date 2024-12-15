df4365b8b4fdd988ba79ff13e11284fb6654c38f
kookong2 <hy010309@naver.com>
2024-12-16 00:11:43 +0900

    [FEAT] 담당자 퀴즈 조회 페이지 라우터 설정 및 페이지 생성

---
+++ src/views/quiz/HrQuizPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>퀴즈 조회</h1>
+        <search-bar></search-bar>
+        <button>퀴즈 등록</button>
+        <white-box>
+            <list-component></list-component>
+        </white-box>
+    </div>
+</template>
+
+<script setup>
+import SearchBar from '@/components/SearchBarComponent.vue';
+import WhiteBox from '@/components/WhiteBoxComponent.vue';
+import ListComponent from '@/components/ListComponent.vue';
+</script>
