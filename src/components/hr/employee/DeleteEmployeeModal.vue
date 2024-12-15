7bb7ad53fe7d2971c6dfa467202b53a2e2c2dc3a
wildcat222 <wildcat222@naver.com>
2024-12-15 23:14:51 +0900

    [DESIGN] 사원 삭제 확인 모달 퍼블리싱

---
+++ src/components/hr/employee/DeleteEmployeeModal.vue
@@ -0,0 +1,96 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="confirmation-container">
+    <h2 class="confirmation-title">해당 사원을 삭제하시겠습니까?</h2>
+    <div class="info-group">
+      <div class="info-row">
+        <span class="info-label">사번</span>
+        <span class="info-value">20140991</span>
+      </div>
+      <div class="info-row">
+        <span class="info-label">부서</span>
+        <span class="info-value">인사팀</span>
+      </div>
+      <div class="info-row">
+        <span class="info-label">직급</span>
+        <span class="info-value">사원</span>
+      </div>
+      <div class="info-row">
+        <span class="info-label">이름</span>
+        <span class="info-value">홍길동</span>
+      </div>
+    </div>
+    <div class="button-group">
+      <button class="delete-button">삭제</button>
+      <button class="cancel-button">취소</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.confirmation-container {
+  padding: 20px;
+  border-radius: 10px;
+  background-color: var(--white);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  text-align: center;
+  justify-items: center;
+}
+
+.confirmation-title {
+  font-size: 20px;
+  font-weight: bold;
+  padding-bottom: 10px;
+  margin-bottom: 20px;
+}
+
+.info-group {
+  width: 60%;
+  margin-bottom: 20px;
+}
+
+.info-row {
+  display: flex;
+  justify-content: left;
+  margin-bottom: 10px;
+}
+
+.info-label {
+  font-weight: bold;
+}
+
+.info-value {
+  margin-left: 50px;
+}
+
+.button-group {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+.delete-button {
+  width: 40%;
+  background-color: var(--purple);
+  color: var(--white);
+  border: none;
+  border-radius: 5px;
+  padding: 10px 15px;
+  cursor: pointer;
+  margin-right: 5px;
+}
+
+.cancel-button {
+  width: 40%;
+  background-color: var(--light-gray);
+  color: var(--black);
+  border: none;
+  border-radius: 5px;
+  padding: 10px 15px;
+  cursor: pointer;
+  margin-left: 5px;
+}
+</style>
