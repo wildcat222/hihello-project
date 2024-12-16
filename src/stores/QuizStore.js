8fc4cf8972143c841da16c9c868114f1e19ac072
kookong2 <hy010309@naver.com>
2024-12-16 22:28:11 +0900

    [FEAT] 퀴즈 조회 pinia 적용

--- src/stores/QuizStore.js
+++ src/stores/QuizStore.js
@@ -3,11 +3,21 @@ import { defineStore } from 'pinia';
 
 export const useQuizStore = defineStore('quizStore', {
   state: () => ({
-    quizCategorySeq: 1, // quizCategorySeq 값 관리 기본값 1로 설정 추후 고쳐야 함
+    quizCategorySeq: 1,
+    quizItems: [],
   }),
   actions: {
     setQuizCategorySeq(seq) {
       this.quizCategorySeq = seq;
     },
+    setQuizItems(items) {
+      this.quizItems = items;
+    },
+    clearQuizItems() {
+      this.quizItems = [];
+    },
+  },
+  persist: {
+    storage: sessionStorage, 
   },
 });
