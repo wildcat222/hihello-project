acd426b0f3b080a9722c5a2b818d1de031bd97a4
minju <kminju0224@gmail.com>
2024-12-17 11:19:35 +0900

    [FEAT] 멘토링 계획서 상세 조회

--- src/router/MentoringRouter.js
+++ src/router/MentoringRouter.js
@@ -1,5 +1,6 @@
 import MentoringPlanningListPage from "@/views/mentoring/MentoringPlanningListPage.vue";
 import MentoringPlanningCreatePage from "@/views/mentoring/MentoringPlanningCreatePage.vue";
+import MentoringPlanningDetailPage from "@/views/mentoring/MentoringPlanningDetailPage.vue";
 export default [
     {
         path: '/mentoring/planning',
@@ -8,5 +9,10 @@ export default [
     {
         path: '/mentoring/planning/create',
         component: MentoringPlanningCreatePage
+    },
+    {
+        path: '/mentoring/planning/:planningSeq',
+        component:MentoringPlanningDetailPage,
+        props: true
     }
 ]
