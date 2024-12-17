a7166bafc1c28e0d2387ebd11c1c550ec72e55f1
minju <kminju0224@gmail.com>
2024-12-17 17:00:01 +0900

    [REFACTOR] mentor, seq 변경

--- src/main/java/spring/hi_hello_spring/employee/query/dto/MentorDepQueryDTO.java
+++ src/main/java/spring/hi_hello_spring/employee/query/dto/MentorDepQueryDTO.java
@@ -11,6 +11,7 @@ import lombok.ToString;
 @RequiredArgsConstructor
 public class MentorDepQueryDTO {
 
+    private Long employeeSeq;
     private String employeeNum;
     private String employeeName;
     private String departmentName;
