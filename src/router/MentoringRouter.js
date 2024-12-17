5af7e0cdd8cdabf05ab314a3ae9c0d0fd25dd54e
minju <kminju0224@gmail.com>
2024-12-17 10:42:46 +0900

    [FIX] 라우터 설정

--- src/router/MentoringRouter.js
+++ src/router/MentoringRouter.js
@@ -1,6 +1,5 @@
 import MentoringPlanningListPage from "@/views/mentoring/MentoringPlanningListPage.vue";
 import MentoringPlanningCreatePage from "@/views/mentoring/MentoringPlanningCreatePage.vue";
-import MentoringPlanningDetailPage from "@/views/mentoring/MentoringPlanningDetailPage.vue";
 export default [
     {
         path: '/mentoring/planning',
@@ -9,10 +8,5 @@ export default [
     {
         path: '/mentoring/planning/create',
         component: MentoringPlanningCreatePage
-    }//,
-    // {
-    //     path: '/mentoring/planning/:planningSeq',
-    //     component:MentoringPlanningDetailPage,
-    //     props: true
-    // }
+    }
 ]
