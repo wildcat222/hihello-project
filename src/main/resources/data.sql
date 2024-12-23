7b5e1703e44aed0b7c4a5946b978e2c247bdfd59
minju <kminju0224@gmail.com>
2024-12-23 11:16:52 +0900

    [FIX] data 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -51,12 +51,12 @@ VALUES
     ('테스트 완료', 2);
 
 -- Mentoring 데이터 삽입
-INSERT INTO mentoring (mentor_seq, mentee_seq, reg_date)
+INSERT INTO mentoring (mentor_seq, mentee_seq, chat_room_seq, reg_date)
 VALUES
-    (5, 1, NOW()),
-    (5, 2, NOW()),
-    (6, 3, NOW()),
-    (6, 4, NOW());
+    (5, 1, 1, NOW()),
+    (5, 2, 2, NOW()),
+    (6, 3, 3, NOW()),
+    (6, 4, 4, NOW());
 
 -- Planning 데이터 삽입
 INSERT INTO planning (employee_seq, planning_name, planning_content, planning_status, reg_date)
@@ -95,10 +95,10 @@ VALUES
     (1, 4, FALSE, NOW());
 
 -- Task Group 데이터 삽입
-INSERT INTO task_group (task_seq, task_group_active_status, reg_date)
+INSERT INTO task_group (task_seq, task_group_active_status, task_group_num, chat_room_seq, reg_date)
 VALUES
-    (1, TRUE, NOW()),
-    (2, TRUE, NOW());
+    (1, TRUE, 1, 5, NOW()),
+    (2, TRUE, 2, 6, NOW());
 
 -- Onboarding Status 데이터 삽입
 INSERT INTO onboarding_status (template_seq, employee_seq, onboarding_completed_status, reg_date)
@@ -112,7 +112,7 @@ VALUES
 INSERT INTO task_submit (task_seq, employee_seq, task_submit_content, reg_date)
 VALUES
     (1, 1, "1차 과제 제출합니다.", NOW()),
-    (1, 2, "제출합니다.", NOW());
+    (1, 2, "2차 과제 제출합니다..", NOW());
 
 -- Group Member 데이터 삽입
 INSERT INTO group_member (task_group_seq, employee_seq)
