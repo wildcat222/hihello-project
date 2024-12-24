325b21fd35aeb78e40bff62d8ff4ab336f5f52d1
minju <kminju0224@gmail.com>
2024-12-24 15:02:52 +0900

    [REFACTOR] kafka 리팩토링

--- src/main/java/spring/hi_hello_spring/chatting/command/application/controller/ChatController.java
+++ src/main/java/spring/hi_hello_spring/chatting/command/application/controller/ChatController.java
@@ -7,55 +7,74 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Controller;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 import spring.hi_hello_spring.chatting.command.application.dto.ChatRequestMessage;
-import spring.hi_hello_spring.chatting.command.application.serivce.ChatMessageListener;
 import spring.hi_hello_spring.chatting.command.application.serivce.ChatRoomService;
+import spring.hi_hello_spring.chatting.command.application.serivce.ChatService;
+import spring.hi_hello_spring.chatting.command.application.serivce.KafkaProducerService;
 import spring.hi_hello_spring.common.response.ApiResponse;
 import spring.hi_hello_spring.common.response.ResponseUtil;
 import spring.hi_hello_spring.chatting.command.application.dto.ChatResponseMessage;
+import spring.hi_hello_spring.common.util.CustomUserUtils;
+
 import java.util.List;
 
 
 @Slf4j
 @RequiredArgsConstructor
-//@RequestMapping("api/v1/chat")
-@Controller // https 사용 x -> restAPI 아님
-@Tag(name = "ChatRequestMessage", description = "채팅 내역 API")
+@RequestMapping("api/v1/chat")
+@RestController // https 사용 x -> restAPI 아님
+@Tag(name = "Chat", description = "채팅 API")
 public class ChatController {
 
-    private final ChatRoomService chatRoomService;
-    private final KafkaTemplate<String, ChatRequestMessage> kafkaTemplate;
-    private final ObjectMapper objectMapper;  // ObjectMapper를 주입받기
-    private final ChatMessageListener chatMessageListener;
+    private final ChatService chatService;
+    private final KafkaProducerService kafkaProducerService;
 
     // 메시지 전송
-    @PostMapping("/{roomId}/sendMessage")
-    public ResponseEntity<Void> sendMessage(@PathVariable String roomId, @RequestBody ChatRequestMessage message) {
+    @MessageMapping("/{roomId}")
+//    @SendTo("/sub/{roomId}")
+    public ResponseEntity<Void> sendMessage(@DestinationVariable String roomId, @Payload ChatRequestMessage message) {
         log.info("Received message: {}", message);
 
-        try {
-            objectMapper.writeValueAsString(message);
-            chatRoomService.saveChatMessage(roomId, message);
-            // Kafka에 메시지 발행 (JSON 형태로)
-            kafkaTemplate.send("chat-messages", roomId, message);
-//            chatMessageListener.sendMessage(message);
+        message.setRoomId(roomId); // mongoDB에 저장
+        ChatResponseMessage savedMessage = chatService.saveMessage(message);
+        log.info("Saved message: {}", savedMessage);
 
-        } catch (Exception e) {
-            log.error("Failed to serialize message", e);
-            return ResponseEntity.status(500).build();  // 직렬화 실패 시 500 오류 반환
-        }
+        // kafka로 메시지 전송
+        kafkaProducerService.sendMessageToKafka(savedMessage);
+//        try {
+//            objectMapper.writeValueAsString(message);
+//            chatRoomService.saveChatMessage(roomId, message);
+//            // Kafka에 메시지 발행 (JSON 형태로)
+//            kafkaTemplate.send("chat-messages", roomId, message);
+////            chatMessageListener.sendMessage(message);
+//
+//        } catch (Exception e) {
+//            log.error("Failed to serialize message", e);
+//            return ResponseEntity.status(500).build();  // 직렬화 실패 시 500 오류 반환
+//        }
 
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/room/{roomId}/message")
-    @ResponseBody
-    @Operation(summary = "채팅 내역", description = "채팅 내역을 반환합니다.")
+    @GetMapping("/{roomId}/messages")
+    @Operation(summary = "채팅 내역", description = "특정 채팅방 내 메시지 조회")
     public ResponseEntity<ApiResponse<List<ChatResponseMessage>>> loadMessage(@PathVariable("roomId") String roomId) {
         // 채팅 내역을 조회하여 반환
-        List<ChatResponseMessage> messages = chatRoomService.chattingMessageList(roomId);
+        // 본인이 속한 것만 가져와야 하는디....  + Util에서 빼오기
+        List<ChatResponseMessage> messages = chatService.chattingMessageList(CustomUserUtils.getCurrentEmployeeSeq() ,roomId);
         return ResponseUtil.successResponse(messages); // 내용 확인용
     }
+
+    @Operation(summary = "채팅 메시지 삭제", description = "채팅 메시지 삭제")
+    @DeleteMapping("/{messageId}")
+    public ResponseEntity<ApiResponse<Void>> deleteMessage(@PathVariable("messageId") String messageId) {
+        chatService.deleteMessage(messageId);
+        return ResponseEntity.ok().build();
+    }
+
 }
