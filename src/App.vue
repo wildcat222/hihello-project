e79da961c4d7742034d02f685ee1bba78593e5c5
wildcat222 <wildcat222@naver.com>
2024-12-26 18:58:27 +0900

    [REFACTOR] 알림창 디테일 작업

--- src/App.vue
+++ src/App.vue
@@ -4,41 +4,11 @@ import {useRoute} from 'vue-router';
 import AsideComponent from '@/components/AsideComponent.vue';
 import {useUserStore} from '@/stores/UserStore';
 import {springAPI} from '@/services/axios.js';
-import EmployeeProfile from "@/components/EmployeeProfile.vue";
 
 const route = useRoute();
 const userStore = useUserStore();
 
 const shouldShowAside = computed(() => !route.meta.hideAside);
-
-const shouldShowProfile = ref(false);
-const activeMenu = ref(null);
-
-// Aside에서 이벤트 발생 시 프로필 활성화/비활성화
-const toggleProfile = (data) => {
-  shouldShowProfile.value = data;
-
-  // Profile 활성화 시 SubMenu 비활성화
-  if (shouldShowProfile.value) {
-    activeMenu.value = null;
-  }
-}
-
-const toggleMenu = (data) => {
-  shouldShowProfile.value = false;
-  activeMenu.value = data;
-}
-
-// 외부 클릭 시 Profile/SubMenu 비활성화
-const hideComponentsOnOutsideClick = (event) => {
-  const profileElement = document.querySelector('.profile');
-  const asideMenuElement = document.querySelector('.aside');
-
-  if (!event.target.closest('.profile') && !event.target.closest('.menu-item')) {
-    shouldShowProfile.value = false;
-    activeMenu.value = null;
-  }
-};
 
 onMounted(() => {
   if (userStore.accessToken) {
@@ -49,25 +19,14 @@ onMounted(() => {
   // Axios 인터셉터 초기화
   userStore.initializeInterceptors();
 
-  // 외부 클릭 이벤트 리스너 등록
-  document.addEventListener('mousedown', hideComponentsOnOutsideClick);
 });
 
-onUnmounted(() => {
-  // 컴포넌트가 언마운트 될 때 이벤트 리스너 제거
-  document.removeEventListener('mousedown', hideComponentsOnOutsideClick);
-});
 </script>
 
 <template>
   <div id="app">
     <!-- AsideComponent 렌더링 여부 결정 -->
-    <AsideComponent class="aside" v-if="shouldShowAside"
-                    @profile-modal="toggleProfile"
-                    :active-menu="activeMenu"
-                    @update-active-menu="toggleMenu"/>
-    <EmployeeProfile class="profile" v-if="shouldShowProfile"
-                     @should-show-profile="toggleProfile"/>
+    <AsideComponent class="aside" v-if="shouldShowAside"/>
     <div class="router-container">
       <router-view />
     </div>
@@ -93,12 +52,5 @@ onUnmounted(() => {
 .aside {
   height: 100%;
   position: relative;
-}
-
-.profile {
-  position: absolute;
-  margin-top: 150px;
-  margin-left: 210px;
-
 }
 </style>
