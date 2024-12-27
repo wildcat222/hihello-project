c0290c5b2103a11061e764b16a12f724217726ea
Whoo <74580387+WhooGeek@users.noreply.github.com>
2024-12-28 03:45:28 +0900

    [FEAT] 회의실 예약 기능 구현

--- src/router/OnboardingRouter.js
+++ src/router/OnboardingRouter.js
@@ -1,5 +1,6 @@
 import OnboardingDataPage from "@/views/onboarding/OnboardingDataPage.vue";
 import BreakApplyPage from "@/views/onboarding/BreakApplyPage.vue";
+import ConferenceRoomPage from "@/views/onboarding/ConferenceRoomPage.vue";
 
 export default [
     {
@@ -9,5 +10,9 @@ export default [
     {
         path: '/break',
         component: BreakApplyPage,
+    },
+    {
+        path: '/onboarding/conferenceRoom',
+        component: ConferenceRoomPage
     }
 ]
