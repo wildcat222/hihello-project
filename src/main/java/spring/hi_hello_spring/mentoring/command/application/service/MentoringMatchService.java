b0c537f31414659408e2552c4b5c2e30e171fbf8
wildcat222 <wildcat222@naver.com>
2024-12-27 15:43:00 +0900

    [REFACTOR] 멘토링 매칭 시 온보디 교육이 시작되며 알림 전송되도록 리팩토링

--- src/main/java/spring/hi_hello_spring/mentoring/command/application/service/MentoringMatchService.java
+++ src/main/java/spring/hi_hello_spring/mentoring/command/application/service/MentoringMatchService.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spring.hi_hello_spring.chatting.command.application.serivce.ChatRoomService;
+import spring.hi_hello_spring.common.util.CustomUserUtils;
 import spring.hi_hello_spring.mentoring.command.application.dto.MentoringGroupDTO;
 import spring.hi_hello_spring.mentoring.command.domain.aggregate.entity.Mentoring;
 import spring.hi_hello_spring.mentoring.command.domain.repository.MentoringRepository;
+import spring.hi_hello_spring.notify.command.application.service.NotifyService;
+import spring.hi_hello_spring.notify.command.domain.aggregate.entity.NotiType;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,11 +19,13 @@ import java.util.UUID;
 public class MentoringMatchService {
 
     private final MentoringRepository mentoringRepository;
+    private final NotifyService notifyService;
     private final ChatRoomService chatRoomService;
 
     @Transactional
     public void createMentoringGroup(List<MentoringGroupDTO> mentoringGroupDTO) {
 
+        Long senderSeq = CustomUserUtils.getCurrentEmployeeSeq();
         for (MentoringGroupDTO mentoringGroupDTOs : mentoringGroupDTO) {
             // MentoringGroup 생성
             Mentoring mentoringGroup = Mentoring.builder()
@@ -31,6 +36,8 @@ public class MentoringMatchService {
 
             Mentoring savedMentoringGroup = mentoringRepository.save(mentoringGroup);
             System.out.println("MentoringGroup saved with mentoringSeq: " + savedMentoringGroup.getMentoringSeq());
+            List<Long> receivers = List.of(mentoringGroup.getMentorSeq(), mentoringGroup.getMenteeSeq());
+            notifyService.send(senderSeq, receivers, NotiType.ONBOARDING_START, "");
 
             // mentoringSeq를 roomId로 사용하여 ChatRoom 생성
             String roomId = savedMentoringGroup.getChatRoomSeq();
