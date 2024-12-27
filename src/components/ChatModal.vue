0f2dbd1333513d1dba6f9c40c4e1a0d986374224
hyomee2 <s5763305@naver.com>
2024-12-27 15:55:18 +0900

    [CHORE] 충돌해결

---
+++ src/components/ChatModal.vue
@@ -0,0 +1,109 @@
+<template>
+  <div v-if="isVisible" class="chat-modal-overlay">
+    <div
+        class="chat-modal"
+        :style="{ left: position.x + 'px', top: position.y + 'px' }"
+        @mousedown="handleMouseDown"
+    >
+      <div class="modal-header">
+        <h3>Chat with us</h3>
+        <button @click="closeModal">X</button>
+      </div>
+      <div class="modal-body">
+        <Chatting/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps, onMounted, onUnmounted } from 'vue';
+import Chatting from "@/views/chat/Chatting.vue";
+
+const props = defineProps({
+  isVisible: Boolean // 모달의 표시 여부를 부모로부터 전달받음
+});
+
+const emit = defineEmits(['update:isVisible']); // 부모로부터 isVisible 업데이트 받을 수 있게 설정
+
+// 위치 변수
+const position = ref({ x: 200, y: 200 });  // 초기 위치 설정
+let isDragging = ref(false); // 드래그 여부
+let dragStartX = ref(0); // 드래그 시작 X 좌표
+let dragStartY = ref(0); // 드래그 시작 Y 좌표
+
+// 모달 닫기
+const closeModal = () => {
+  emit('update:isVisible', false); // 부모 컴포넌트에서 isVisible을 false로 변경하여 모달을 숨김
+};
+
+// 드래그 시작
+const handleMouseDown = (event) => {
+  isDragging.value = true;
+  dragStartX.value = event.clientX - position.value.x;
+  dragStartY.value = event.clientY - position.value.y;
+
+  // 마우스 커서가 draggable된 모달 내에서 움직여도 스크롤이 발생하지 않도록 막기
+  document.body.style.userSelect = 'none';
+};
+
+// 드래그 중 위치 업데이트
+const handleMouseMove = (event) => {
+  if (isDragging.value) {
+    position.value.x = event.clientX - dragStartX.value;
+    position.value.y = event.clientY - dragStartY.value;
+  }
+};
+
+// 드래그 종료
+const handleMouseUp = () => {
+  isDragging.value = false;
+  document.body.style.userSelect = '';  // drag 종료 후 userSelect 원래대로 복원
+};
+
+// 마우스 이벤트 설정
+onMounted(() => {
+  window.addEventListener('mousemove', handleMouseMove);
+  window.addEventListener('mouseup', handleMouseUp);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('mousemove', handleMouseMove);
+  window.removeEventListener('mouseup', handleMouseUp);
+});
+</script>
+
+<style scoped>
+.chat-modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.chat-modal {
+  background-color: var(--white);
+  width: 400px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  cursor: move;
+  position: absolute;
+  height: 500px;
+}
+
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  border-bottom: 1px solid var(--gray);
+}
+
+.modal-body {
+  padding: 20px;
+}
+</style>
