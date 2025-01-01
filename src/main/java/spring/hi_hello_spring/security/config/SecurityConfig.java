880c48465af602f90df3bb521f3021bfeafa6dc7
minju <kminju0224@gmail.com>
2025-01-02 02:32:37 +0900

    [REFACTOR] 채팅 관련 시큐리티

--- src/main/java/spring/hi_hello_spring/security/config/SecurityConfig.java
+++ src/main/java/spring/hi_hello_spring/security/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
             "/swagger-ui/",
             "/v3/api-docs.yaml",
             "/",
+            "/ws/**",
             "/api/v1/test/health",
             "/actuator/prometheus"
     };
