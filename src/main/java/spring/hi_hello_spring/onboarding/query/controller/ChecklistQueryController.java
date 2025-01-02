8c52ce26bec6082d4b471ebadb28ad6a97ff787a
hyomee2 <s5763305@naver.com>
2025-01-03 03:27:25 +0900

    [FIX] 체크리스트 모두 체크 시 온보딩 수행 상태 변경 오류 해결

--- src/main/java/spring/hi_hello_spring/onboarding/query/controller/ChecklistQueryController.java
+++ src/main/java/spring/hi_hello_spring/onboarding/query/controller/ChecklistQueryController.java
@@ -3,13 +3,15 @@ package spring.hi_hello_spring.onboarding.query.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import spring.hi_hello_spring.common.exception.CustomException;
+import spring.hi_hello_spring.common.exception.ErrorCodeType;
 import spring.hi_hello_spring.common.response.ApiResponse;
 import spring.hi_hello_spring.common.response.ResponseUtil;
+import spring.hi_hello_spring.common.util.CustomUserUtils;
+import spring.hi_hello_spring.employee.query.service.EmployeeQueryService;
 import spring.hi_hello_spring.onboarding.query.service.ChecklistQueryService;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -18,11 +20,21 @@ import spring.hi_hello_spring.onboarding.query.service.ChecklistQueryService;
 public class ChecklistQueryController {
 
     private final ChecklistQueryService checklistQueryService;
+    private final EmployeeQueryService employeeQueryService;
 
     @GetMapping("/template/{templateSeq}/checklist/status")
     @Operation(summary = "체크리스트 수행 완료 상태 조회", description = "체크리스트 수행 완료 상태 조회 로직입니다.")
-    public ApiResponse<?> getChecklistStatus(@PathVariable("templateSeq") Long templateSeq) {
-        boolean checklistStatus = checklistQueryService.getChecklistStatusByTemplateSeq(templateSeq);
+    public ApiResponse<?> getChecklistStatus(@PathVariable("templateSeq") Long templateSeq, @RequestParam String userRole) {
+        Long menteeSeq;
+
+        if (Objects.equals(userRole, "MENTEE")) {
+            menteeSeq = CustomUserUtils.getCurrentEmployeeSeq();
+        } else if (Objects.equals(userRole, "MENTOR")) {
+            menteeSeq = employeeQueryService.getMenteeInfo().getEmployeeSeq();
+        } else {
+            throw new CustomException(ErrorCodeType.DATA_NOT_FOUND);
+        }
+        boolean checklistStatus = checklistQueryService.getChecklistStatusByTemplateSeq(templateSeq, menteeSeq);
         return ResponseUtil.successResponse("체크리스트 수행 완료 상태가 성공적으로 조회되었습니다.", checklistStatus).getBody();
     }
 }
