84d92203e0a2d061535bfb1c42ec45b064de86d5
kookong2 <hy010309@naver.com>
2025-01-06 23:56:48 +0900

    [REFACTOR] poetry 실행 스크립트 수정

--- app/main.py
+++ app/main.py
@@ -20,17 +20,18 @@ app = FastAPI()
 ENV = os.getenv("production", "development")  # 기본값은 'development'
 
 # CORS 설정
+# CORS 설정
 if ENV == "development":
-    origins = ["http://localhost:5173", "http://127.0.0.1:5173"]  # 개발 환경 도메인
+    origins = ["http://localhost:5173", "http://127.0.0.1:5173"]
 else:
-    origins = os.getenv("ALLOWED_ORIGINS", "https://hi-hello.site").split(",")  # 배포 환경 도메인
+    origins = os.getenv("ALLOWED_ORIGINS", "https://hi-hello.site").split(",")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://hi-hello.site"],  # 허용할 출처
-    allow_credentials=True,  # 인증 정보 허용 (쿠키 등)
-    allow_methods=["*"],  # 모든 HTTP 메서드 허용
-    allow_headers=["*"],  # 모든 헤더 허용
+    allow_origins=origins,  # 환경 변수에서 가져온 origins 사용
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # 글로벌 예외 핸들러 등록
