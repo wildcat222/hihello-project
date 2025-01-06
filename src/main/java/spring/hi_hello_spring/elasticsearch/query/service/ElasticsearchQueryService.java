030189649792038ed39ae6e862f532579f838837
hyomee2 <s5763305@naver.com>
2025-01-06 12:37:31 +0900

    [CHORE] 엘라스틱 서치 로그 코드 추가

--- src/main/java/spring/hi_hello_spring/elasticsearch/query/service/ElasticsearchQueryService.java
+++ src/main/java/spring/hi_hello_spring/elasticsearch/query/service/ElasticsearchQueryService.java
@@ -1,6 +1,7 @@
 package spring.hi_hello_spring.elasticsearch.query.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @ConditionalOnProperty(name = "spring.data.elasticsearch.repositories.enabled", havingValue = "true")
+@Slf4j
 public class ElasticsearchQueryService {
 
     private final WikiDocumentRepository wikiDocumentRepository;
@@ -20,6 +22,7 @@ public class ElasticsearchQueryService {
     /* 엘라스틱 서치 관련 로직 */
     // 위키 검색 로직
     public List<WikiDocument> searchWiki(String keyword) {
+        log.info("위키 검색 진입");
         return wikiDocumentRepository.searchByWikiTitle(keyword);
     }
 }
