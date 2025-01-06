846e6a1a89b4eb0b26815d129f76cedcacf08240
wildcat222 <wildcat222@naver.com>
2025-01-06 12:51:39 +0900

    [REFACTOR] 베포 시 활성화 옵션 주석 처리

--- src/main.js
+++ src/main.js
@@ -9,6 +9,16 @@ import '@fortawesome/fontawesome-free/css/all.css';
 
 document.title = import.meta.env.VITE_APP_TITLE || 'Default Title';
 
+// console 로그 필터링 설정 (베포 환경에선 주석 풀기)
+// const originalConsole = {...console}
+// window.console = {
+//     ...originalConsole,
+//     log: () => {},    // log 메시지 숨김
+//     info: () => {},   // info 메시지 숨김
+//     warn: () => {},   // warning 메시지 숨김
+//     error: originalConsole.error  // error는 유지
+// }
+
 const app = createApp(App);
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedState);
